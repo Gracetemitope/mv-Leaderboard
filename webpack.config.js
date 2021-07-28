07eb2b4928e7ca40281bcad06d0f3275cf42a969
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-07-27 23:16:37 -0700

    complete project set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,49 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const StylelintPlugin = require('stylelint-webpack-plugin');
+// const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  /* eslint-disable */
+
+  // plugins: [new StylelintPlugin()],
+  // plugins: [new ESLintPlugin()],
+  /* eslint-enable */
+
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  /* eslint-disable */
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Document',
+      template: './src/index.html',
+    }),
+  ],
+  /* eslint-enable */
+
+  output: {
+    filename: '[name].bundle.js',
+
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: './',
+
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
 };
