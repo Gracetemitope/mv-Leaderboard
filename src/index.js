a1977a41bd9bb7ef770fc3f971ebf4b729f01499
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-07-29 22:42:36 -0700

    integrate leaderboard API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,61 @@
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { displayUI } from './UI';
+let refreshBtn = document.getElementById('refresh_btn')
+const leaderBoard = document.getElementById("leader-board")
 
-function app() {
-  displayUI()
+function getAllScores() {
+  return new Promise((resolve) => {
+    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lcWYwv0iaXerpaxrmmIU/scores/')
+      .then((response) => response.json())
+      .then(resolve);
+  });
 }
-app()
+export default async function getScores() {
+  const setVal = await getAllScores();
+  return setVal;
+}
+
+refreshBtn.addEventListener('click',(e)=>{
+  e.preventDefault();
+  const resultJSON = getScores();
+  resultJSON.then((data) => {
+    console.log(data.result);
+  data.result.forEach(element => {
+    const p = document.createElement('p');
+    p.innerText = `${element.user}: ${element.score}`;
+    leaderBoard.appendChild(p);
+  });
+    });
+})
+
+
+function fetchData (userName, scorePoint) {
+  return new Promise((resolve)=>{
+    fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lcWYwv0iaXerpaxrmmIU/scores/", {
+    method: 'POST',
+    body: JSON.stringify({
+      user: userName,
+      score: scorePoint
+    }), 
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  })
+  .then((res) => res.json())
+  .then((json) => console.log(json));
+  resolve('saved game')
+  })
+} 
+  
+document.getElementById("leader-board").addEventListener("submit", (e) => {
+  e.preventDefault()
+  console.log("clicked")
+  let nameInput = document.getElementById("name_input").value;
+  let scoreInput = document.getElementById("score_input").value;
+  fetchData(nameInput, scoreInput)
+  const board = document.createElement("p")
+  board.innerHTML = `${nameInput}  ${scoreInput}`
+
+  leaderBoard.appendChild(board)
+
+})
